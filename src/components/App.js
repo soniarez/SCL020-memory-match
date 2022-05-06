415eb1aeec37069be0ee2278dde78e5df79440a8
AlejandraMogollon <alejandrame1505@gmail.com>
2022-05-06 09:13:56 -0400

    probando Test

--- src/components/App.js
+++ src/components/App.js
@@ -17,71 +17,67 @@
 import pokemon from "../data/pokemon/pokemon.js";
 //console.log(pokemon)
 
-
 //FUNCIÓN SCORE
 const drawScoreSetValue = (score) => {
-  console.log('score', score)
+  console.log("score", score);
   // let labelScore = document.getElementById("score");
   return "Score: " + score * 10;
 };
 
-const clearSelectCards = (selectedCards) =>{
+const clearSelectCards = (selectedCards) => {
   selectedCards = [];
   return selectedCards;
-}
-const clearSelectedCardsNames = (selectedCardsNames) =>{
+};
+const clearSelectedCardsNames = (selectedCardsNames) => {
   selectedCardsNames = [];
-  return selectedCardsNames
-}
-const sumScore = (score)=>{
+  return selectedCardsNames;
+};
+const sumScore = (score) => {
   score += 1;
   return score;
-}
+};
 
-const sound = (path) =>{
-  if(path){
+const sound = (path) => {
+  if (path) {
     let matchSound = new Audio(path);
     matchSound.play();
   }
   return true;
-}
+};
 //FUNCIÓN SHUFFLING CARTAS
-const shuffle = (pokemon) => { 
+const shuffle = (pokemon) => {
   let duplicate = pokemon.concat(pokemon);
-  duplicate.sort(() => Math.random() - 0.5); 
+  duplicate.sort(() => Math.random() - 0.5);
   return duplicate; //retornará array de cartas aleatoriamente
 };
-let time =  '';
-let timerEl = '';
+let timerEl = "";
 
 let secs = 0;
 let mins = 0;
 let SS = "";
 let MM = "";
-const getTime = () =>{ 
-    secs++;
-    if (secs == 60) {
-      secs = 0;
-      mins++;
-    }
+const getTime = () => {
+  secs++;
+  if (secs == 60) {
+    secs = 0;
+    mins++;
+  }
 
-    secs < 10 ? (SS = `0${secs}`) : (SS = `${secs}`);
-    mins < 10 ? (MM = `0${mins}`) : (SS = `${mins}`);
+  secs < 10 ? (SS = `0${secs}`) : (SS = `${secs}`);
+  mins < 10 ? (MM = `0${mins}`) : (SS = `${mins}`);
 
-    timerEl = "Timer: " + `${MM}:${SS}`;
-    console.log(timerEl, 'timerEl')
-    return timerEl; 
-}
- 
+  timerEl = "Timer: " + `${MM}:${SS}`;
+  console.log(timerEl, "timerEl");
+  return timerEl;
+};
 
- 
 //export default App;
-export {  
-  drawScoreSetValue, 
+export {
+  drawScoreSetValue,
   clearSelectCards,
   clearSelectedCardsNames,
   sumScore,
   sound,
-  shuffle, 
-  getTime
+  shuffle,
+  getTime,
 };
--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -7,55 +7,28 @@
   });
 }); */
 
-import { shuffle, start, playGame, gameBoard } from "./App.js";
-import pokemon from "../data/pokemon/pokemon.js";
+import {
+  // drawScoreSetValue,
+  // clearSelectCards,
+  // clearSelectedCardsNames,
+  // sumScore,
+  // sound,
+  shuffle,
+  // getTime,
+} from "./App.js";
 
-describe("App.js", () => {
-  let pokemons = pokemon.items;
-  it("should return a shuffle deck", () => {
-    // const names = ["Miguel", "Alejandra", "Gabriela"];
-    // const nameConcat = [
-    //   "Miguel",
-    //   "Alejandra",
-    //   "Gabriela",
-    //   "Miguel",
-    //   "Alejandra",
-    //   "Gabriela",
-    // ];
-    const shuffleArr = shuffle(pokemons);
-    expect(shuffleArr).toHaveLength(18);
-    expect(shuffleArr).not.toEqual(pokemons);
-  });
-  //--Shuffle
-  it("should be a function", () => {
-    expect(typeof shuffle).toBe("function");
-  });
-  it("should properly shuffle data", () => {
-    expect(shuffle(pokemons)).not.toEqual(pokemons);
-  });
-
-  //--Start 
-  it("should be a function", () => {
-    expect(typeof start).toBe("function");
-  });
-  it("should render without crashing", () => {
-    //console.log(game.start());
-    expect(start()[0] instanceof HTMLElement).toBe(true);
-  });
-  //--playGame
-  it("should be a function", () => {
-    expect(typeof playGame).toBe("function");
-  });
-  //no sé cómo testetar esta parte, el test encuentra problema en el evento de card.addEventListerner.
-  //no sé si es que primero debemos testear esa parte, cuando se da el primer click
-  it.skip("should return true when comparing two cards that are a match", () => {
-    //console.log(document);
-    const cardtest = `<div class="card is-flipped" data-name="blastoise"><img class="front" src="https://www.serebii.net/pokemongo/pokemon/009.png"><img class="back" src="img/backcard.png"></div>`;
-    expect(playGame(cardtest)).toBe(true);
-  });
-  //--gameBoard
-  it("Should bring a Array.length > 0", () => {
-    expect(gameBoard(pokemons).length > 1);
-    expect(gameBoard(pokemons)[0] instanceof HTMLElement);
+describe("Testing Functions", () => {
+  it("Should return a shuffle deck", () => {
+    const nameConcat = [
+      "Sonia",
+      "Heike",
+      "Alejandra",
+      "Sonia",
+      "Heike",
+      "Alejandra",
+    ];
+    const shuffleArr = shuffle(nameConcat);
+    expect(shuffleArr).toHaveLength(6);
+    expect(shuffleArr).not.toEqual(nameConcat);
   });
 });
